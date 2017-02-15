b7cefb206f3ea9fca60e62cdb516255182601c70
Dennis Caldwell <dennis@bipedalprogrammer.com>
2017-02-14 20:58:23 -0500

    Initial project as created by initializer.

---
+++ src/main/java/com/bipedalprogrammer/journal/ReactJournalApplication.java
@@ -0,0 +1,12 @@
+package com.bipedalprogrammer.journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactJournalApplication.class, args);
+	}
+}
