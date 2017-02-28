f4c160feaee2080b8c240bb4fc034d0c80d70d89
Dennis Caldwell <dennis@bipedalprogrammer.com>
2017-02-27 21:47:13 -0500

    Added material-ui for front end development.

--- src/main/java/com/bipedalprogrammer/journal/controller/JournalController.java
+++ src/main/java/com/bipedalprogrammer/journal/controller/JournalController.java
@@ -1,7 +1,12 @@
 package com.bipedalprogrammer.journal.controller;
 
+import com.bipedalprogrammer.journal.model.Journal;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by bipedal on 2/15/17.
@@ -10,4 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path="/journal")
 public class JournalController {
 
+    @RequestMapping(path="/", method=RequestMethod.GET)
+    public List<Journal> getJournals() {
+        return new ArrayList<Journal>();
+    }
 }
--- src/main/js/app.js
+++ src/main/js/app.js
@@ -2,6 +2,9 @@
 
 const React = require('react');
 const ReactDOM = require('react-dom');
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 class App extends React.Component {
     constructor(props) {
@@ -11,10 +14,14 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>Nothing to see here.</div>
+            <MuiThemeProvider>
+                <AppBar title="Bipedal Journal" />
+            </MuiThemeProvider>
         )
     }
 }
+
+injectTapEventPlugin();
 
 ReactDOM.render(
     <App />,
